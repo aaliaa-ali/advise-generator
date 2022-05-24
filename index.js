f5d9544dfe3fb224ddefb16cac9511f74ca2b5a7
aaliaa-ali <aaliaa32amr@gmail.com>
2022-05-24 15:37:31 +0200

    add onload function

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ let adviceId = document.getElementsByClassName("id")[0];
 let id = 1;
 
 adviceBtn.addEventListener("click", getAdvice);
+window.addEventListener("load", getAdvice, false);
 
 async function getAdvice() {
-    try{
-        let responce = await fetch(`https://api.adviceslip.com/advice/${id}`);
-        let advice = await responce.json();
-        let { id: adviceId, advice: adviceContent } = advice.slip;
-        console.log(advice, adviceContent, id);
-        renderAdvice(adviceId, adviceContent);
-        id++;
-    }catch{
-
-    }
-  
+  try {
+    randNum = Math.random() * 100 + 1;
+    id = Math.floor(randNum);
+    console.log(id);
+    let responce = await fetch(`https://api.adviceslip.com/advice/${id}`);
+    let advice = await responce.json();
+    let { id: adviceId, advice: adviceContent } = advice.slip;
+    console.log(advice, adviceContent, id);
+    renderAdvice(adviceId, adviceContent);
+    id++;
+  } catch {}
 }
 
 function renderAdvice(id, adviceContent) {
